29f8a92aee4ac836da2b0a0a76d75bfa1f5cd8b0
Samuel Wolf <samuelx5n@yahoo.de>
2019-01-24 17:43:30 +0100

    small improvements -> no longer 2d list required

--- algorithms/sorting algorithms/terminus.py
+++ algorithms/sorting algorithms/terminus.py
@@ -26,14 +26,14 @@ def plot(x_values, y_values):
 
 def set_parser(prs):
     prs.add_option("-t", "--set_title", type="string", help="set title of figure", dest="t")
-    prs.add_option("-x", "--x_values", type="string", help="x values, 2d list expected", dest="x")
-    prs.add_option("-y", "--y_values", type="string", help="y values, 2d list expected", dest="y")
+    prs.add_option("-x", "--x_values", type="string", help="x values, \"[a, b, c], [d, e, f]\"", dest="x")
+    prs.add_option("-y", "--y_values", type="string", help="y values, \"[a, b, c], [d, e, f]\"", dest="y")
     prs.add_option("--set_ylabel", type="string", help="sets a label on the y axis", dest="yl")
     prs.add_option("--set_xlabel", type="string", help="sets a label on the x axis", dest="xl")
 
 
 def convert_string_input_to_list(strng):
-    return [[int(j) for j in i[1:len(i)-1].split(",")] for i in re.findall("\[.*?\]", strng[1:len(strng)-1])]
+    return [[int(j) for j in i[1:len(i)-1].split(",")] for i in re.findall("\[.*?\]", strng)]
 
 
 def main():
